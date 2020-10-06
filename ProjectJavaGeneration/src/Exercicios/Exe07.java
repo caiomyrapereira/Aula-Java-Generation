4d095e8516248f149eabdef32e38a8639b332719
caio myra <caiomyrapereira@hotmail.com>
2020-10-06 17:53:11 -0300

    Desafios finalizado.

---
+++ ProjectJavaGeneration/src/Exercicios/Exe07.java
@@ -0,0 +1,34 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exe07 {
+	public static void main(String args[]) {
+		//7. Um sistema de equações lineares do tipo:
+		
+		Scanner ler = new Scanner(System.in);
+		float a, b, c, d, e, f, x, y;
+		
+		System.out.println("Digite um valor qualquer na variavel 'A' :");
+		a = ler.nextInt();
+		System.out.println("Digite um valor qualquer na variavel 'B' :");
+		b = ler.nextInt();
+		System.out.println("Digite um valor qualquer na variavel 'C' :");
+		c = ler.nextInt();
+		System.out.println("Digite um valor qualquer na variavel 'D' :");
+		d = ler.nextInt();
+		System.out.println("Digite um valor qualquer na variavel 'E' :");
+		e = ler.nextInt();
+		System.out.println("Digite um valor qualquer na variavel 'F' :");
+		f = ler.nextInt();
+		
+		x = ( c*e - b*f ) / ( a*e - b*d );
+		y = ( a*f - c*d ) / ( a*e - b*d );
+		
+		System.out.println("O valor do x é ");
+		System.out.println(x);
+		System.out.println("O valor do y é ");
+		System.out.println(y);
+		
+	}
+}
