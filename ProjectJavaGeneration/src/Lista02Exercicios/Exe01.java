418487bfb6437ecdb5aed7e9bb49c55678d487d2
caio myra <caiomyrapereira@hotmail.com>
2020-10-07 13:39:42 -0300

    Add Exercicios 1,2 e 4 na lista02

---
+++ ProjectJavaGeneration/src/Lista02Exercicios/Exe01.java
@@ -0,0 +1,34 @@
+package Lista02Exercicios;
+
+import java.util.Scanner;
+
+public class Exe01 {
+	public static void main(String args[]) {
+		
+		// 1- Faça um programa que receba três inteiros e diga qual deles é o maior.
+		
+		Scanner ler = new Scanner(System.in);
+		int n1, n2, n3;
+			
+		System.out.print("Digite um valor qualquer na variavel 'n1' :");
+		n1 = ler.nextInt();
+		System.out.print("Digite um valor qualquer na variavel 'n2' :");
+		n2 = ler.nextInt();
+		System.out.print("Digite um valor qualquer na variavel 'n3' :");
+		n3 = ler.nextInt();
+		
+		if( (n1>n2) && (n1>n3) ) {
+			System.out.printf("A variavel 'n1' é maior do que 'n2' e 'n3'.");
+		}
+		else if((n2>n1) && (n2>n3)) {
+			System.out.printf("A variavel 'n2' é maior do que 'n1' e 'n3'.");
+		}
+		else if((n3>n1) && (n3>n2)) {
+			System.out.printf("A variavel 'n3' é maior do que 'n1' e 'n2'.");
+		}
+		else {
+			System.out.printf("\n O maior numero é inválido, pois existe dois ou três numeros maiores que é iguais.");
+		}
+		
+	}
+}
