f6e6491b729bb7ae388e6e473c24005ca6c3d067
caio myra <caiomyrapereira@hotmail.com>
2020-10-07 11:24:05 -0300

    Renomeado o nome do pacote.

---
+++ ProjectJavaGeneration/src/Lista01Exercicios/Exe03.java
@@ -0,0 +1,33 @@
+package Lista01Exercicios;
+
+import java.util.Scanner;
+
+public class Exe03 {
+	public static void main(String args[]) {
+		/*
+		  3. Faça um sistema que leia o tempo de duração de um evento em uma fábrica
+		     expressa em segundos e mostre-o expresso em horas, minutos e segundos.
+		*/
+		
+		Scanner ler = new Scanner(System.in);
+		
+		int totalSegs, horas, mins, segs;
+		
+		System.out.println("Digite a duração de um evento em uma fábrica em segundos: ");
+		totalSegs = ler.nextInt();
+		
+		horas = (totalSegs/60)/60;
+		mins  = (totalSegs/60)%60;
+		segs  = totalSegs%60;
+		
+		
+		System.out.println("Horas:");
+		System.out.println(horas);
+		System.out.println("Minutos:");
+		System.out.println(mins);
+		System.out.println("Segundos:");
+		System.out.println(segs);
+		
+		
+	}
+}
---
+++ ProjectJavaGeneration/src/Lista01Exercicios/Exe06.java
@@ -0,0 +1,33 @@
+package Lista01Exercicios;
+
+import java.util.Scanner;
+
+public class Exe06 {
+	public static void main(String args[]){
+		/*
+		  6. Construa um programa em c que, tendo como dados de entrada dois pontos
+             quaisquer no plano, P(x1, y1) e P(x2, y2), escreva a distância entre eles. A fórmula
+             que efetua tal cálculo.
+		*/
+		
+		Scanner ler = new Scanner(System.in);
+		float x1, y1, x2, y2;
+		double d;
+		
+		System.out.println("Digite a sua primeiro ponto do x1: ");
+		x1 = ler.nextFloat();
+		System.out.println("Digite a sua primeiro ponto do y1: ");
+		y1 = ler.nextFloat();
+		System.out.println("Digite a sua primeiro ponto do x2: ");
+		x2 = ler.nextFloat();
+		System.out.println("Digite a sua primeiro ponto do y2: ");
+		y2 = ler.nextFloat();
+		
+		d = Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2) );
+		
+		System.out.println("A distancia entre dois ponto é  ");
+		System.out.println(d);
+
+		
+	}
+}
